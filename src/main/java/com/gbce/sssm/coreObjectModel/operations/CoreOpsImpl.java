d9e199cfdaaf0e89b281ee10d0807d0b3625309d
richard-diamond <rjd@diamond.id.au>
2018-07-12 16:52:00 +0100

    implemented dividend yield functionality

--- src/main/java/com/gbce/sssm/coreObjectModel/operations/CoreOpsImpl.java
+++ src/main/java/com/gbce/sssm/coreObjectModel/operations/CoreOpsImpl.java
@@ -10,10 +10,62 @@ import com.gbce.sssm.coreObjectModel.data.Stock;
 
 public class CoreOpsImpl implements CoreOps {
 
+    private static final int ROUNDING_MODE   = BigDecimal.ROUND_HALF_DOWN;
+    private static final int ROUNDING_DIGITS = 4;
+
+
+
     @Override
     public BigDecimal calculateDividendYield(Stock stock,
                                              long  price) {
 
-        return null;
+        BigDecimal yield;
+
+        if (stock == null)
+            yield = null;                           // TODO: need to determine the business requirement for this scenario (exception?)
+        else if (stock.getStockType() == null)
+            yield = null;                           // TODO: need to determine the business requirement for this scenario (exception?)
+        else
+            switch (stock.getStockType()) {
+                case COMMON:
+                    yield = divideAndRound(stock.getLastDividend(), price);
+                    break;
+
+                case PREFERRED:
+                    if (stock.getFixedDividend() == null)
+                        yield = BigDecimal.ZERO;    // TODO: need to determine the business requirement for this scenario
+                    else
+                        yield = divideAndRound(stock.getFixedDividend() * stock.getParValue(), price);
+                    break;
+
+                default:
+                    yield = BigDecimal.ZERO;        // TODO: need to determine the business requirement for this scenario
+            }
+
+        return yield;
+    }
+
+
+
+    private BigDecimal divideAndRound(long dividend,
+                                      long divisor) {
+
+        return divideAndRound(new BigDecimal(dividend), new BigDecimal(divisor));
+    }
+
+
+
+    private BigDecimal divideAndRound(double dividend,
+                                      long   divisor) {
+
+        return divideAndRound(new BigDecimal(dividend), new BigDecimal(divisor));
+    }
+
+
+
+    private BigDecimal divideAndRound(BigDecimal dividend,
+                                      BigDecimal divisor) {
+
+        return dividend.divide(divisor, ROUNDING_DIGITS, ROUNDING_MODE);
     }
 }
